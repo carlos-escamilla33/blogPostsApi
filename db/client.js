a4fc6a538487252d051f505f5791ebb6db29e21b
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-01-02 21:16:18 -0800

    created client file and index

---
+++ db/client.js
@@ -0,0 +1,10 @@
+const {Client} = require("pg");
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/blogposts-dev',
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+});
+
+module.exports = {
+    client,
+}
